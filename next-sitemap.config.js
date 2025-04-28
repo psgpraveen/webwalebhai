134679623c1c6dbd8c6d9f0fd55f5ef8657ce741
Praveen Kumar Gupta <pg23403@gmail.com>
2025-04-28 11:38:45 +0530

    Update

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://webwalebhai.vercel.app",
+    generateRobotsTxt: true,
+    generateSitemap: true,
+    priority: 0.7,
+    changefreq: "weekly",
+  };
+  
